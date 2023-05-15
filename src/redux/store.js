98e574b499b06f21f8f316d5334518f9f1f72e2c
YuraStarchenko <100568416+YuraStarchenko@users.noreply.github.com>
2023-05-15 07:58:17 +0300

    +

--- src/redux/store.js
+++ src/redux/store.js
@@ -11,7 +11,7 @@ import {
 } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import { contactsReducer } from './contacts/contactsSlice';
-import { authReducer } from './auth/slice';
+import { authReducer } from './auth/authSlice';
 
 const middleware = [
   ...getDefaultMiddleware({
